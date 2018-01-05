5a425055097087e27d1df9ed59e1e4064045e828
Kishy-nivas <kishynivas10@gmail.com>
2018-01-05 15:29:56 +0530

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -6,14 +6,14 @@ class InsertionSort
     int j,key;
     for(int i = 1; i < size; i++)
     {
-      key = arr[i];
+      key = arr[i];       // moving the values backwards will erase the value arr[i], so store it in key 
       j = i-1; 
       while (j >=0 && arr[j] > key)
       {
-        arr[j+1] = arr[j];
+        arr[j+1] = arr[j];  // move the value backwards to make space for the key 
         j--;
       }
-      arr[j+1] = key;
+      arr[j+1] = key;      // insert the key in the correct position 
 
 
     }
