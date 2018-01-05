f0bbe6d61c0105651ac0ef747ef4ca8344766bf6
Kishy-nivas <kishynivas10@gmail.com>
2018-01-05 12:29:53 +0530

    added sorting section

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+class RemoveDuplicates{
+  public static int remove(int[] arr){
+    int size = arr.length; 
+    int j = 0;                       
+    for(int i = 0;i < size;i++){
+      if (arr[i] != arr[j]){                // if same value, j holds the last reference of the unique numbers 
+        j++;
+        arr[j] = arr[i];
+      }
+    }
+    return j+1;
+  }
+
+  public static void main(String[] args){
+    int[] arr= {
+      1,2,3,3,3,3,4
+    };
+    int new_size = remove(arr);
+    for(int i = 0; i < new_size; i++){
+      System.out.println( arr[i] );
+
+    }
+
+  }
+}
---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,56 @@
+class BubbleSort{
+  private static void BubbleSortBrute(int[] arr){
+    int size = arr.length; 
+    for(int i = 0; i < size; i++){
+      for (int j = 0 ; j < size-i-1; j++){                 //-i ,because upto i are already sorted, -1 to avoid range error 
+        if (arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+
+        }
+      }
+    }
+    for(int i : arr){
+      System.out.println(i);
+    }
+
+  }
+
+  private static void BubbleSortOptimized(int[] arr){
+    int size = arr.length;
+    boolean swapped; 
+    for(int i = 0; i < size; i++){
+      swapped = false; 
+      for(int j = 0; j < size-i-1; j++){
+        if(arr[j] > arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          swapped = true ;
+        }
+      }
+      if (swapped == false ){          //if no swap, then the arr is already sorted, so just break the loop.
+        break;
+      }
+    }
+    for(int i : arr){
+      System.out.println(i);
+    }
+
+  }
+
+
+
+
+  public static void main(String[] args){
+    int[] arr = {
+      7,6,5,4,3,2,1
+    };
+    BubbleSortBrute(arr);
+    int[] arr1= {
+      1,2,3,4,5,6,7
+    };
+    BubbleSortOptimized(arr1);
+  }
+}
